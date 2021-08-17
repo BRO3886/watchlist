1959ee16da9bb7a092676bd04e4b96e5cb1af588
Siddhartha Varma <siddverma1999@gmail.com>
2021-08-17 23:07:02 +0530

    feat: create basic project structure and add packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
+  //This is the glue that binds the framework to the Flutter engine
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // set orientation lock
+  const List<DeviceOrientation> orientations = [
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ];
+  await SystemChrome.setPreferredOrientations(orientations);
+
   runApp(WatchList());
 }
